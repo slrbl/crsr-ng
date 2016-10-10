9d270a0b88e654be621d68c8870f3960ae864651
Walid Daboubi <walid.daboubi@gmail.com>
2016-10-10 19:32:35 +0200

    module3-solution

---
+++ module3-solution/app.js
@@ -0,0 +1,87 @@
+(function () {
+
+  'use strict';
+  angular.module('NarrowItDownApp',[])
+  .controller('NarrowItDownController',NarrowItDownController)
+  .service('MenuSearchService',MenuSearchService)
+
+  .directive('foundItems', foundItems);
+
+  function foundItems() {
+  var ddo = {
+    templateUrl: 'foundItems.html',
+    scope: {
+      myItems:'=',
+      title: '@',
+    },
+
+    controller: NarrowItDownController,
+    controllerAs: 'NarrowItDown',
+    bindToController:{
+      onRemove: '&'
+    },
+  };
+  return ddo;
+}
+
+
+
+  NarrowItDownController.$inject=['$scope','MenuSearchService'];
+  function NarrowItDownController($scope,MenuSearchService) {
+    var NarrowItDown=this;
+
+    NarrowItDown.found=[]
+
+
+
+
+    NarrowItDown.getMatchedMenuItems=function () {
+      NarrowItDown.found=MenuSearchService.getMatchedMenuItems(NarrowItDown.searched);
+    }
+
+    NarrowItDown.removeItem = function (itemIndex) {
+      NarrowItDown.title=NarrowItDown.found[itemIndex].name+" was removed"
+      NarrowItDown.found.splice(itemIndex, 1);
+
+
+  }
+
+
+  }
+
+
+
+
+
+
+
+
+  MenuSearchService.$inject=['$http']
+  function MenuSearchService($http) {
+    var service=this;
+    service.getMatchedMenuItems=function(searchTerm){
+      service.filtredResult=[];
+      var response = $http({
+        method: "GET",
+        url: "https://davids-restaurant.herokuapp.com/menu_items.json"
+      });
+      response.then(function (response) {
+        service.items=response.data;
+        console.log(service.items);
+        for (var i=0; i<response.data.menu_items.length; i++) {
+          if (response.data.menu_items[i].description.indexOf(searchTerm) !== -1){
+            service.filtredResult.push(response.data.menu_items[i]);
+          }
+        }
+      })
+      .catch(function (error) {
+        console.log("Something went terribly wrong.");
+        service.filtredResult=[];
+      });
+      return service.filtredResult;
+    };
+  }
+
+
+
+})();
